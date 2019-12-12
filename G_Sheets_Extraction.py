16507571558c2010e206e8ec321571e076ba7d50
James Lovie <james.lovie1@gmail.com>
2019-12-12 16:19:56 +1100

    Updated output filename.

--- G_Sheets_Extraction.py
+++ G_Sheets_Extraction.py
@@ -26,7 +26,7 @@ class Gsheets_Extractor:
 
 	def extract_data(self, workbook):
             start_page_number = 148
-            npp_data_final = pd.DataFrame()
+            data_final = pd.DataFrame()
 
             total_sheets = len(workbook.worksheets())
             print(f'There are: {total_sheets} sheets in the workbook.')
@@ -34,7 +34,7 @@ class Gsheets_Extractor:
             sheet = workbook.get_worksheet(start_page_number)
             values = sheet.get_all_values()
             npp_data = pd.DataFrame(values[1:],columns=values[0])
-            npp_data_final = pd.concat([npp_data_final, npp_data], ignore_index=True, sort=False)
+            data_final = pd.concat([data_final, npp_data], ignore_index=True, sort=False)
 
             for x in range(start_page_number, total_sheets):
                 # Google API limits: https://developers.google.com/sheets/api/limits
@@ -43,11 +43,11 @@ class Gsheets_Extractor:
                 values = sheet.get_all_values()
                 npp_data = pd.DataFrame(values[1:],columns=values[0])
                 # Append the nth record data to the master dataframe.
-                npp_data_final = pd.concat([npp_data_final, npp_data], ignore_index=True, sort=False)
+                data_final = pd.concat([data_final, npp_data], ignore_index=True, sort=False)
                 print(f'Completed sheet # {x}')
 
             self.total_sheets = total_sheets
-            return npp_data_final
+            return data_final
 
 
 	def save_data(self, dataframe, filename):
@@ -61,11 +61,11 @@ def main():
     workbook = extractor.google_api_connector()
 
     # Call the function extract_data.
-    npp_data_final = extractor.extract_data(workbook)
+    data_final = extractor.extract_data(workbook)
     print(f'Have extracted {extractor.total_sheets} sheets sucessfully')
 
     # Call the function save_data.
-    extractor.save_data(npp_data_final, 'npp_data_november_2019.csv')
+    extractor.save_data(data_final, 'data_november_2019.csv')
 
 if __name__ == '__main__':
     main()
